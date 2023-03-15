0a85974fc91f6c3f0dbaecaab0ab56d3b8e6daae
dearasheville <dearasheville@outlook.com>
2023-03-15 19:26:45 +0500

    new brain-prime game implementation added

---
+++ src/games/prime.js
@@ -0,0 +1,24 @@
+import startNewGame from '../index.js';
+import { getRandomNumber } from '../utils.js';
+
+const isPrime = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const primeGame = () => {
+  const randomNumber = getRandomNumber();
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
+
+  const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const gameQuestion = `Question: ${randomNumber}\n`;
+
+  return [correctAnswer, gameDescription, gameQuestion];
+};
+
+export default () => startNewGame(primeGame);
